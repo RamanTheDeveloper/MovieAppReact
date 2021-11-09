65e67cfe04e49b0a009964fa31864a39ee59b8af
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2021-11-09 17:21:02 +0100

    Hook created to fetch movie for movie page

--- movie-app-react/src/components/Movie.js
+++ movie-app-react/src/components/Movie.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+//Config
+import {IMAGE_BASE_URL, POSTER_SIZE} from '../config';
+//Components
+import Grid from '../components/Grid';
+import Spinner from '../components/Spinner';
+//Hooks
+import { useMovieFetch } from "../hooks/useMovieFetch";
+//Image
+import NoImage from '../images/no_image.jpg';
 
-const Movie = () => <div>Movie</div>
+const Movie = () => {
+    const {movieId} = useParams();//Take movie ID from url
+
+    const {state: Movie, loading, error} = useMovieFetch(movieId)
+    return(
+        <>
+            <div>Movie</div>
+        </>
+    );
+}
 
 export default Movie;
